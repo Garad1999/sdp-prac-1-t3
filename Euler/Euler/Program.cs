e4d587c1f559b6745905b8b33feb7d19acf12875
Marc <g18m6731@campus.ru.ac.za>
2020-02-16 15:12:48 +0200

    Euler Palindrome Product 4

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -2,11 +2,40 @@
 
 namespace Euler
 {
-    class Program
-    {
+    class Program 
+   {
+	public static int Largest_Palindrome_Product()
+	{
+		int upper = 999;
+		int lower = 100;
+
+		int max_product = 0; //initialize result storage
+
+		for (int i = upper; i>= lower; i--) //loop from highest numbers downward
+		{
+			for (int j = i; j >= lower; j--)
+			{
+				int product = i*j;
+				if (product<max_product)
+					break;
+				int n = product;
+				int reverse = 0;
+
+				while (n != 0)
+				{
+					reverse = reverse*10 + n % 10;
+					n /= 10;
+				}
+
+				if (product == reverse && product>max_product)
+					max_product = product;
+			}			
+		}
+		return max_product;
+	}
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Largest_Palindrome_Product());
         }
     }
 }
